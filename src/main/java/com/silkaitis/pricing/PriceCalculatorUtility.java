11c553442d938f27c5cfa1439e0ea64bcff19ce2
Spinxas <spinxas@gmail.com>
2019-05-16 12:33:13 +0100

    Refactoring.

--- src/main/java/com/silkaitis/pricing/PriceCalculatorUtility.java
+++ src/main/java/com/silkaitis/pricing/PriceCalculatorUtility.java
@@ -35,33 +35,58 @@ public class PriceCalculatorUtility implements PriceCalculator {
             int frequency = Collections.frequency(products, product);
 
             //Check for three for two discounts.
-            if (frequency >= 3 && discountService.getThreeForTwoDiscount(product.getName())) {
-                int discountApplicable = frequency / 3;
-
-                //Calculates the discount by multiplying the price by three and subtracting the result of multiplication by two.
-                //The discount is multiplied by the frequency of repeating products of threes.
-                BigDecimal discount = (product.getPrice().multiply(BigDecimal.valueOf(THREE))
-                        .subtract(product.getPrice().multiply(BigDecimal.valueOf(TWO))))
-                        .multiply(BigDecimal.valueOf(discountApplicable));
-                System.out.println("Three for Two discount: " + discount);
-
-                total = total.subtract(discount);
-            }
+            total = threeForTwoDiscount(total, product, frequency);
 
             //Check for two for one discounts.
-            if (frequency >= 2 && discountService.getTwoForOneDiscount(product.getName())) {
-                int discountApplicable = frequency / 2;
-
-                //Calculates the discount by multiplying the price by the frequency of repeating products of twos.
-                BigDecimal discount = (product.getPrice().multiply(BigDecimal.valueOf(discountApplicable)));
-
-                System.out.println("Two for One discount: " + discount);
-                total = total.subtract(discount);
-            }
-
+            total = twoForOneDiscount(total, product, frequency);
         }
 
         System.out.println("Total to Pay: " + total);
+        return total;
+    }
+
+    /**
+     * Checks for Two for One discount.
+     * @param total - sub total amount.
+     * @param product - given product.
+     * @param frequency - frequency of this order within shopping cart.
+     * @return - discount.
+     * @throws LogicException - Any logical errors while retrieving discount.
+     */
+    private BigDecimal twoForOneDiscount(BigDecimal total, Product product, int frequency) throws LogicException {
+        if (frequency >= 2 && discountService.getTwoForOneDiscount(product.getName())) {
+            int discountApplicable = frequency / 2;
+
+            //Calculates the discount by multiplying the price by the frequency of repeating products of twos.
+            BigDecimal discount = (product.getPrice().multiply(BigDecimal.valueOf(discountApplicable)));
+
+            System.out.println("Two for One discount: " + discount);
+            total = total.subtract(discount);
+        }
+        return total;
+    }
+
+    /**
+     * Checks for Three for Two discount.
+     * @param total - sub total amount.
+     * @param product - given product.
+     * @param frequency - frequency of this order within shopping cart.
+     * @return - discount.
+     * @throws LogicException - Any logical errors while retrieving discount.
+     */
+    private BigDecimal threeForTwoDiscount(BigDecimal total, Product product, int frequency) throws LogicException {
+        if (frequency >= 3 && discountService.getThreeForTwoDiscount(product.getName())) {
+            int discountApplicable = frequency / 3;
+
+            //Calculates the discount by multiplying the price by three and subtracting the result of multiplication by two.
+            //The discount is multiplied by the frequency of repeating products of threes.
+            BigDecimal discount = (product.getPrice().multiply(BigDecimal.valueOf(THREE))
+                    .subtract(product.getPrice().multiply(BigDecimal.valueOf(TWO))))
+                    .multiply(BigDecimal.valueOf(discountApplicable));
+            System.out.println("Three for Two discount: " + discount);
+
+            total = total.subtract(discount);
+        }
         return total;
     }
 }
